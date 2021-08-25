2a7664b4c125ff41367dbc2ff7f79f151c964667
Miguel Alvarez <ing.migueljalvarez@gmail.com>
2021-08-25 03:48:32 -0500

    se agrega informacion un poco mas detallada del pokemon

--- src/components/PokemonsDetails.jsx
+++ src/components/PokemonsDetails.jsx
@@ -1,14 +1,15 @@
 import React from "react";
-import { Modal, Button } from "react-bootstrap";
+import { Modal, Button, ProgressBar } from "react-bootstrap";
 import { useSelector } from "react-redux";
 import { capitalize } from "../helpers/capitalize";
 
 const PokemonsDetails = ({ show, onHide }) => {
   const { pokemon } = useSelector((state) => state);
-  const specie = pokemon.types.map((data) => data.type.name) || ["desconocida"];
-  const habilities = pokemon.abilities.map((data) => data.ability.name) || [
-    "desconocida",
-  ];
+  const defaultImages =
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/0.png";
+
+  const specialDefense = pokemon.stats ? pokemon.stats["special-defense"] : 0;
+  const specialAttack = pokemon.stats ? pokemon.stats["special-attack"] : 0;
 
   return (
     <Modal
@@ -25,20 +26,60 @@ const PokemonsDetails = ({ show, onHide }) => {
       </Modal.Header>
       <Modal.Body>
         <div className="d-flex justify-content-around">
-          <div>
-            <p>Especie: {capitalize(specie.join(" "))} </p>
+          <div id="pokemon-info" className="">
+            <div>
+              <span>hp:</span>
+              <ProgressBar
+                now={pokemon.stats?.hp || 0}
+                label={`${pokemon.stats?.hp || 0}%`}
+              />
+            </div>
+            <div>
+              <span>attack:</span>
+              <ProgressBar
+                now={pokemon.stats?.attack || 0}
+                label={`${pokemon.stats?.attack || 0}%`}
+              />
+            </div>
+            <div>
+              <span>defense:</span>
+              <ProgressBar
+                now={pokemon.stats?.defense || 0}
+                label={`${pokemon.stats?.defense || 0}%`}
+              />
+            </div>
+            <div>
+              <span>special-attack:</span>
+              <ProgressBar now={specialAttack} label={`${specialAttack}%`} />
+            </div>
+            <div>
+              <span>special-defense:</span>
+              <ProgressBar now={specialDefense} label={`${specialDefense}%`} />
+            </div>
+
             <p>Peso: {pokemon.weight} </p>
-            <p>Habilidades: {habilities.join(" ")} </p>
+            <p>Habilidades: {pokemon?.abilities?.join(" ")} </p>
           </div>
 
-          <div className="">
-            <img src={pokemon.sprites.front_default} alt={pokemon.name} />
-            <p className="text-center">{capitalize(pokemon.name)}</p>
+          <div className="text-center">
+            <img
+              src={pokemon.imageUrl}
+              alt={pokemon.name}
+              onError={(e) => {
+                e.target.onerror = null;
+                e.target.src = defaultImages;
+              }}
+              className="m-auto"
+            />
+            <h3 className="text-center">{capitalize(pokemon.name)}</h3>
+            <h4>Especie: {capitalize(pokemon.type)} </h4>
           </div>
         </div>
       </Modal.Body>
       <Modal.Footer>
-        <Button variant="danger" onClick={onHide}>Close</Button>
+        <Button variant="danger" onClick={onHide}>
+          Close
+        </Button>
       </Modal.Footer>
     </Modal>
   );
