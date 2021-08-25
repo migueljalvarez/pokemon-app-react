d5d82e121fe687163989b43af931e35e1ec8c302
Miguel Alvarez <ing.migueljalvarez@gmail.com>
2021-08-24 22:43:21 -0500

    se remueven los warnins para poder publicar la primera version

--- src/pages/Pokemons.jsx
+++ src/pages/Pokemons.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, memo } from "react";
+import React, { useEffect, useState } from "react";
 import PokemonDetails from "../components/PokemonsDetails";
 import { currentPokemon, allPokemons } from "../redux/actions";
 import { useDispatch, useSelector } from "react-redux";
